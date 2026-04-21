3e414be65a8e6a93b8eb1986005044647175d392
TheBanthaFodder <micahhurzeler@gmail.com>
2026-04-21 15:42:37 -0400

    add skeleton code structure for RacerModel Class

---
+++ src/main/java/model/RacerModel.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class RacerModel{
+
+    public void raceRegistration(){}
+
+    public void signUp(){}
+
+    public void BuyLicense(){}
+
+    public void reviewRace(){}
+
+}
