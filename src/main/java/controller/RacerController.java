f760a94ebe4387e4e511994440b5f29a06b377b6
TheBanthaFodder <micahhurzeler@gmail.com>
2026-04-21 11:28:48 -0400

    add skeleton code structure to RacerController

---
+++ src/main/java/controller/RacerController.java
@@ -0,0 +1,15 @@
+public class RacerController{
+
+    public void handleSystemSignUp(){}
+
+    public void handleBuyLicense(){}
+
+    public void handleRaceRegistration(){}
+
+    public void handleReviewRace(){}
+
+    public void handleAccount(){}
+
+    public void handleErrors(){}
+
+}
