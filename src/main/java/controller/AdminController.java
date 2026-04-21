52722b790ae24cc1eb570864c530c68172a9656c
TheBanthaFodder <micahhurzeler@gmail.com>
2026-04-21 11:22:34 -0400

    add skeleton structure for AdminController

---
+++ src/main/java/controller/AdminController.java
@@ -0,0 +1,13 @@
+public class AdminController{
+
+    public void handleUserAccounts(){}
+
+    public void handleManageLicenses(){}
+
+    public void handleSystemSettings(){}
+
+    public void handleAccount(){}
+
+    public void handleErrors(){}
+
+}
